0c35f0498af52c4af04f7fe02467425ea33715a5
zthornhill <zthornhill@unomaha.edu>
2021-01-31 15:46:15 -0600

    First Push

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+   <div id= "app">
+    <Skills />
+  </div>
+ 
+</template>
+
+<script>
+import Skills from './components/Skills.vue'
+export default {
+  name: 'app',
+  components: {
+    Skills
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,700');
+
+body {
+  background-color: #EEEEEE;
+  font-family: 'Montserrat', sans-serif;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  align-items: center;
+}
+body, html {
+  margin: 0;
+  height: 100%;
+}
+#app {
+    width: 50%;
+}
+</style>
+
